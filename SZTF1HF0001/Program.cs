b5675860e79c71772df912b1a35e7a05b3c1e284
Yamakasi <Yamaksi@home>
2021-09-21 14:03:40 +0200

    hf1

---
+++ SZTF1HF0001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZTF1HF0001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SZTFHF");
+        }
+    }
+}
