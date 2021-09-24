9c02e7d1d0280188ef122a69f557a8171dd8a8e1
Yamakasi <Yamaksi@home>
2021-09-24 20:31:53 +0200

    sztf1

--- SZTF1HF0001/Program.cs
+++ SZTF1HF0001/Program.cs
@@ -10,7 +10,26 @@ namespace SZTF1HF0001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SZTFHF");
+            int S = 491;
+            int P = 268;
+            int[] szamjegyek = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            for( int i=S; i<S+(P*2); i+=2)
+            {
+                int szam = i;
+                while (szam > 0)
+                {
+                    ++szamjegyek[szam % 10];
+                    szam = (int)(szam / 10);
+                }
+            }
+
+            for( int i=0; i<10; i++)
+            {
+                Console.Write(szamjegyek[i]);
+                if (i < 9)
+                    Console.Write(", ");
+            }
         }
     }
 }
