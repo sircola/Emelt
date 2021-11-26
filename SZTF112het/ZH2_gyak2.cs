3598aae84153b2ceef5cd37d2527633f6f3c1edc
Yamakasi <Yamaksi@home>
2021-11-26 18:46:28 +0100

    a

---
+++ SZTF112het/ZH2_gyak2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHandler.srcPath = "MONITORING.DAT";
+            FileHandler.dstPath = "ANALYSIS.RESULT";
+            Analyzer analyzer = new Analyzer();
+            analyzer.Analysis();
+        }
+    }
+
+    static class FileHandler
+    {
+        public static string srcPath { get; set; }
+        public static string dstPath { get; set; }
+
+        public static string[] Load()
+        {
+            StreamReader sr = new StreamReader(srcPath);
+
+            int i = 0;
+            string[] lines = new string[int.Parse(sr.ReadLine())];
+
+            while (!sr.EndOfStream)
+            {
+                lines[i] = sr.ReadLine();
+                i++;
+            }
+
+            sr.Close();
+
+            return lines;
+        }
+
+        public static void Append(string line)
+        {
+            StreamWriter sw = new StreamWriter(dstPath, true);
+            sw.WriteLine(line);
+            sw.Close();
+        }
+    }
+
+    class Analyzer
+    {
+        Phone[] phones;
+
+        public Analyzer()
+        {
+            string[] p = FileHandler.Load();
+            phones = new Phone[p.Length];
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                string[] d = p[i].Split(':');
+                phones[i] = new Phone(d[0], d[1], d[2].Split(';'));
+            }
+        }
+
+        public void Analysis()
+        {
+            for (int i = 0; i < phones.Length; i++)
+            {
+                if (OSCause(phones[i])) FileHandler.Append(phones[i].ToString());
+                else if (ACCause(phones[i])) FileHandler.Append(phones[i].ToString());
+                else FileHandler.Append(phones[i].ToString());
+            }
+        }
+
+        bool OSCause(Phone phone)
+        {
+            for (int i = 0; i < phone.BatteryLevels.Length - 1; i++)
+            {
+                if (phone.BatteryLevels[i] == 30 & phone.BatteryLevels[i + 1] < 5)
+                {
+                    phone.FC = FaultCause.OS;
+                    return true;
+                }
+            }
+            return false;
+        }
+        bool ACCause(Phone phone)
+        {
+            for (int i = 1; i < phone.BatteryLevels.Length; i++)
+            {
+                if (phone.BatteryLevels[i - 1] > 7 + phone.BatteryLevels[i])
+                {
+                    phone.FC = FaultCause.AC;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    enum FaultCause { OS, AC, OK }
+    class Phone
+    {
+        public string ID { get; }
+        public string Type { get; }
+        public int[] BatteryLevels { get; }
+        public FaultCause FC { get; set; }
+            
+        public Phone(string id, string type, string[] batteryLevel)
+        {
+            ID = id;
+            Type = type;
+
+            BatteryLevels = new int[batteryLevel.Length];
+            for (int i = 0; i < batteryLevel.Length; i++)
+            {
+                BatteryLevels[i] = int.Parse(batteryLevel[i]);
+            }
+
+            FC = FaultCause.OK;
+        }
+
+        public string ToString()
+        {
+            return ID + ":" + FC;
+        }
+    }
+}
---
+++ SZTF112het/ora12.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+
+namespace Ora12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBookDb db = new MyBookDb();
+            User béla = db.CreateUser("Béla", UserRole.Admin, "123456");
+            for (int i = 0; i < 10; i++)
+                béla.Post($"Béla {i}. post...");
+
+            User gizi = db.CreateUser("Gizi", UserRole.User, "qwerty");
+            for (int i = 0; i < 10; i++)
+                gizi.Post($"Gizi {i}. post...");
+
+            User admin = db.LoadUser("myadmin", UserRole.Admin, "password");
+            for (int i = 0; i < 10; i++)
+                admin.Post($"Admin {i}. post...");
+
+            User invalid = db.LoadUser("", UserRole.Admin, "");
+            for (int i = 0; i < 10; i++)
+                invalid.Post($"Invalid {i}. post...");
+        }
+    }
+    enum UserRole { Visitor, User, Admin }
+    class User
+    {
+        public UserRole UserRole { get; private set; }
+        public string UserName { get; private set; }
+        public string UserFolder { get; private set; }
+
+        public User(UserRole role, string name) : this(role, name, "") { }
+        public User(UserRole role, string name, string folder)
+        {
+            UserRole = role;
+            UserName = name;
+            UserFolder = folder;
+
+            if (!string.IsNullOrWhiteSpace(UserFolder) && !Directory.Exists(UserFolder))
+                Directory.CreateDirectory(UserFolder);
+        }
+
+        public void Post(string message)
+        {
+            string path = Path.Combine(UserFolder, DateTime.Now.ToString("yyyy-MM-dd"));
+            switch (UserRole)
+            {
+                //case UserRole.User:
+                //    File.AppendAllText(path, $"[USER][{DateTime.Now}] {message}\n");
+                //    break;
+                //case UserRole.Admin:
+                //    File.AppendAllText(path, $"[ADMIN][{DateTime.Now}] {message}\n");
+                //    break;
+                case UserRole.User:
+                case UserRole.Admin:
+                    File.AppendAllText(path, $"[{UserRole.ToString().ToUpper()}][{DateTime.Now}] {message}\n");
+                    break;
+            }
+        }
+    }
+    class MyBookDb
+    {
+        string db, path;
+
+        public MyBookDb() : this("mybook") { }
+        public MyBookDb(string dbName)
+        {
+            db = dbName;
+            if (!Directory.Exists(db))
+                Directory.CreateDirectory(db);
+
+            path = Path.Combine(db, "myusers.mydb");
+            if (!File.Exists(path))
+                CreateUser("myadmin", UserRole.Admin, "password");
+        }
+        public User CreateUser(string name, UserRole role, string password)
+        {
+            File.AppendAllText(path, $"{name}#{(int)role}#{password}\n");
+            return new User(role, name, Path.Combine(db, name));
+        }
+        public bool IsValidUser(User user, string password)
+        {
+            return IsValidUser(user.UserName, user.UserRole, password);
+        }
+        public bool IsValidUser(string name, UserRole role, string password)
+        {
+            string[] users = File.ReadAllLines(path);
+            for (int i = 0; i < users.Length; i++)
+            {
+                string[] user = users[i].Split('#');
+                if (user[0] == name && int.Parse(user[1]) == (int)role && user[2] == password)
+                    return true;
+            }
+            return false;
+        }
+        public User LoadUser(string name, UserRole role, string password)
+        {
+            if (IsValidUser(name, role, password))
+                return new User(role, name, Path.Combine(db, name));
+            return new User(UserRole.Visitor, name, Path.Combine(db, name));
+        }
+    }
+}
