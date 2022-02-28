cf5d1fc6bd5f50da8e96cd92fc75d7d5154faf47
Yamakasi <Yamaksi@home>
2022-02-28 01:20:14 +0100

    a

---
+++ SZTF23het/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+
+
+namespace SZTF23het
+{
+    public delegate void RendelesTeljesitesKezelo(string etelNeve);
+    public delegate void HozzavaloSzuksegesKezelo(string hozzavalo);
+    public delegate void HozzavaloElkeszultKezelo(string hozzavalo);
+    public delegate void HozzavaloKevesKezelo(KorlatosSzakacs sz, string hozzavalo);
+
+    class Etel
+    {
+        public string megnevezes { get; set; }
+        public string[] hozzavalok { get; set; }
+
+        public Etel(string n, string[] h) 
+        {
+            megnevezes = n;
+            hozzavalok = h;
+        }
+    }
+
+    class Sef
+    {
+        private Etel cel;
+        private int szuksegesHozzavaloSzam;
+
+        Etel[] receptek = new Etel[] 
+        {
+            new Etel("poharviz", new string[] { "viz" } ),
+            new Etel("leves", new string[] { "repa", "hus", "krumpli", "viz" } ),
+            new Etel("rantothus", new string[] { "hus", "krumpli" } ),
+            new Etel("fozelek", new string[] { "viz", "repa" } )
+        };
+
+        public event RendelesTeljesitesKezelo RendelesTeljesitve;
+        public event RendelesTeljesitesKezelo RendelesNemTeljesitheto;
+        public event HozzavaloSzuksegesKezelo HozzavaloSzukseges;
+
+        public void Megrendeles(string etelNeve)
+        {
+            int j = -1;
+            for (int i = 0; i < receptek.Length; i++)
+            {
+                if (receptek[i].megnevezes == etelNeve)
+                    j = i;
+            }
+
+            if (j == -1)
+                RendelesNemTeljesitheto?.Invoke(etelNeve);
+            else
+                Elkeszites(receptek[j]);
+
+        }
+
+        public void Elkeszites(Etel recept)
+        {
+            cel = recept;
+            szuksegesHozzavaloSzam = recept.hozzavalok.Length;
+
+            for (int i = 0; i < szuksegesHozzavaloSzam; i++)
+            {
+                HozzavaloSzukseges?.Invoke(cel.hozzavalok[i]);
+            }
+        }
+
+        public void SzakacsElkeszult(string hozzavalo)
+        {
+            --szuksegesHozzavaloSzam;
+
+            if (szuksegesHozzavaloSzam == 0)
+                RendelesTeljesitve?.Invoke(cel.megnevezes);
+        }
+
+        public void Felvesz(Szakacs szakacs)
+        {
+            if( szakacs is KorlatosSzakacs)
+            {
+                (szakacs as KorlatosSzakacs).HozzavaloNemKeszithetoEl += SzakacsHibatJelez;
+            }
+
+            HozzavaloSzukseges += szakacs.SefKerValamit;
+            szakacs.HozzavaloElkeszult += SzakacsElkeszult;
+        }
+
+        public void SzakacsHibatJelez(string hozzavalo)
+        {
+            Console.WriteLine($"Séf: Szakács hibát jelzett '{hozzavalo}'");
+            RendelesNemTeljesitheto?.Invoke(cel.megnevezes);
+        }
+    }
+
+    public class Szakacs
+    {
+        public string nev { get;  }
+        public string specialitas { get; set; }
+
+        public Szakacs(string n, string s)
+        {
+            nev = n;
+            specialitas = s;
+        }
+
+        public HozzavaloElkeszultKezelo HozzavaloElkeszult;
+
+        public void SefKerValamit(string hozzavalo)
+        {
+            if (hozzavalo == specialitas)
+                Foz();
+        }
+
+        public virtual void Foz()
+        {
+            HozzavaloElkeszult?.Invoke(specialitas);
+        }
+     }
+
+    public class KorlatosSzakacs : Szakacs
+    {
+        public int mennyiseg { get; set; }
+
+        public event HozzavaloElkeszultKezelo HozzavaloNemKeszithetoEl;
+        public event HozzavaloKevesKezelo HozzavaloKeves;
+
+        public KorlatosSzakacs(string nev, string specialitas, int mennyiseg)
+            : base(nev, specialitas)
+        {
+            this.mennyiseg = mennyiseg;
+        }
+
+        public override void Foz()
+        {
+            if (mennyiseg == 0)
+            {
+                HozzavaloNemKeszithetoEl?.Invoke(specialitas);
+                return;
+            }
+
+            --mennyiseg;
+
+            if (mennyiseg < 2)
+                HozzavaloKeves?.Invoke(this,specialitas);
+
+            base.Foz();
+        }
+    }
+
+    class Raktar
+    {
+        public int mennyiseg { get; set; }
+        public string hozzavalo { get; set; }
+
+        public Raktar(string h, int m)
+        {
+            mennyiseg = m;
+            hozzavalo = h;
+        }
+
+        public void Kiad(KorlatosSzakacs sz, string s)
+        {
+            if (mennyiseg == 0)
+                return;
+
+            if (hozzavalo != sz.specialitas)
+                return;
+
+            ++sz.mennyiseg;
+
+            --mennyiseg;
+
+            Console.WriteLine($"Raktár: '{sz.nev}' segítséget kér - hozzávaló: '{sz.specialitas}'");
+
+        }
+    }
+
+    class Program
+    {
+        static void RendelesTeljesitve(string s)
+        {
+            Console.WriteLine($"* Sikeres rendelés {s}");
+        }
+
+        static void RendelesNemTeljesitheto(string s)
+        {
+            Console.WriteLine($"* Sikertelen rendelés {s}");
+        }
+
+        static void Main(string[] args)
+        {
+            Sef sef = new Sef();
+            sef.RendelesTeljesitve += new RendelesTeljesitesKezelo(RendelesTeljesitve);
+            sef.RendelesNemTeljesitheto += RendelesNemTeljesitheto;
+            Szakacs szakacs = new Szakacs("Béla", "viz");
+            KorlatosSzakacs korlatosszakacs = new KorlatosSzakacs("Róbert", "repa", 2);
+            Raktar raktar = new Raktar("repa", 2);
+            korlatosszakacs.HozzavaloKeves += raktar.Kiad;
+            sef.Felvesz(szakacs);
+            sef.Felvesz(korlatosszakacs);
+            sef.Megrendeles("poharviz");
+            sef.Megrendeles("kecskesajt");
+            sef.Megrendeles("fozelek");
+            sef.Megrendeles("fozelek");
+            sef.Megrendeles("fozelek");
+            sef.Megrendeles("fozelek");
+            sef.Megrendeles("fozelek");
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ SZTF23het/Óra/03_Delegate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _03_Esemenykezeles
+{
+    delegate double Közvetítő(double szám);
+    delegate double Közvetítő2(double szám1, double szám2); // (2)
+    delegate void SzövegFeldolgozó(string szöveg); // (3)
+
+    static class Műveletek
+    {
+        public static double Kétszerezés(double szám) { return szám * szám; }
+        public static double Felezés(double szám) { return szám / 2; }
+
+        public static double Szorzás(double szám1, double szám2) { return szám1 * szám2; } // (2)
+
+        public static void KiírNagy(string s) { Console.WriteLine(s.ToUpper()); } // (3)
+        public static void KiírKicsi(string s) { Console.WriteLine(s.ToLower()); }
+    }
+
+    class Delegate
+    {
+        public static void Test1()
+        {
+            Console.WriteLine(new string('-', 80));
+
+            Közvetítő közvetítő = new Közvetítő(Műveletek.Kétszerezés);
+            Console.WriteLine(közvetítő(5));
+
+            közvetítő = new Közvetítő(Műveletek.Felezés);
+            Console.WriteLine(közvetítő(5));
+
+            //Közvetítő2 közvetítő2 = new Közvetítő2(Műveletek.Felezés); // nem egyezik a paraméterezés
+            Közvetítő2 közvetítő2 = new Közvetítő2(Műveletek.Szorzás);
+            Console.WriteLine(közvetítő2(8, 9));
+        }
+        public static void Test2()
+        {
+            Console.WriteLine(new string('-', 80));
+
+            Közvetítő közvetítő = new Közvetítő(Műveletek.Kétszerezés);
+            közvetítő += new Közvetítő(Műveletek.Felezés); // ennek az eredménye lesz a hívás kimenete...
+            // így sok értelme nincsen a visszatérési értéknek
+
+            Console.WriteLine(közvetítő(5));
+        }
+        public static void Test3()
+        {
+            Console.WriteLine(new string('-', 80));
+
+            SzövegFeldolgozó teszt = new SzövegFeldolgozó(Műveletek.KiírNagy);
+            teszt += new SzövegFeldolgozó(Műveletek.KiírKicsi);
+            teszt += new SzövegFeldolgozó(Műveletek.KiírNagy);
+            teszt("Teszt Üzenet");
+            Console.WriteLine(new string('*', 40));
+            teszt -= new SzövegFeldolgozó(Műveletek.KiírNagy); // utolsó egyezőt szedi le
+            teszt("Teszt Üzenet");
+            Console.WriteLine(new string('*', 40));
+            teszt -= new SzövegFeldolgozó(Műveletek.KiírNagy);
+            teszt("Teszt Üzenet");
+            Console.WriteLine(new string('*', 40));
+            teszt -= new SzövegFeldolgozó(Műveletek.KiírNagy); // nincs kivétel ha nincsen olyan
+            teszt("Teszt Üzenet");
+            teszt = null;
+            // visszahívási funkció -> 'Undo'
+        }
+    }
+}
