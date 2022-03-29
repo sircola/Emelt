96011240be461d4d40d745832829b587496fa677
Yamakasi <Yamaksi@home>
2022-03-29 05:43:48 +0200

    a

---
+++ SZTF27het/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SZTF27het
+{
+    class NincsIlyenKivétel : Exception
+    {
+
+    }
+
+    class Szuperhős
+    {
+        public string Név { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
