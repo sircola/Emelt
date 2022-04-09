f1d67c350fa01c4fcd2745bcbc7a0d710e265644
Yamakasi <Yamaksi@home>
2022-04-09 23:27:06 +0200

    a

---
+++ SZTF29het/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace SZTF29het
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SZTF2HF/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SZTF2HF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
