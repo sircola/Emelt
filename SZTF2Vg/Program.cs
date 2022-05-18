25774e908c2b4197a0431b330ad66edb000245e3
Yamakasi <Yamaksi@home>
2022-05-18 15:19:48 +0200

    a

--- SZTF2Vg/Program.cs
+++ SZTF2Vg/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace SZTF2Vg
 {
+
+    abstract class Absz
+    {
+        public int walami { get; set; }
+    }
+
+    class Lesz : Absz
+    {
+        public int valami { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Lesz a = new Lesz();
+            Absz b = (Absz)a;
+            Absz c = a as Absz;
         }
     }
 }
